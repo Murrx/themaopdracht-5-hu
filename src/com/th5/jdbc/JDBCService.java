059470b9ab15a42e1f7da9ebcbf80f2ba584414a
Robin Altena <altenarobin@gmail.com>
2013-09-11 23:17:10 +0200

    basic login functions added that work on database

--- src/com/th5/jdbc/JDBCService.java
+++ src/com/th5/jdbc/JDBCService.java
@@ -1,53 +1,53 @@
-package com.th5.dbtest;
+package com.th5.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
- 
-public class OracleJDBC {
- 
-	public static void main(String[] argv) {
- 
+
+public class JDBCService {
+
+	public static void main(String[]args){
+		getConnection();
+	}
+	
+	public static Connection getConnection() {
+
 		System.out.println("-------- Oracle JDBC Connection Testing ------");
- 
+
 		try {
- 
+
 			Class.forName("oracle.jdbc.driver.OracleDriver");
- 
+
 		} catch (ClassNotFoundException e) {
- 
+
 			System.out.println("Where is your Oracle JDBC Driver?");
 			e.printStackTrace();
-			return;
- 
+
 		}
- 
+
 		System.out.println("Oracle JDBC Driver Registered!");
- 
+
 		Connection connection = null;
- 
+
 		try {
-			connection = DriverManager.getConnection("jdbc:oracle:thin:@//ondora01.hu.nl:8521/cursus01.hu.nl","stud1566357","team5");
- 
+			connection = DriverManager.getConnection(
+					"jdbc:oracle:thin:@//ondora01.hu.nl:8521/cursus01.hu.nl",
+					"tho5_2013_2a_team5", "team5iscool");
+
 		} catch (SQLException e) {
- 
+
 			System.out.println("Connection Failed! Check output console");
 			e.printStackTrace();
-			return;
- 
+
 		}
- 
+
 		if (connection != null) {
 			System.out.println("You made it, take control your database now!");
-			try {
-				connection.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			return connection;
 		} else {
 			System.out.println("Failed to make connection!");
+			return null;
 		}
 	}
- 
+
 }
