346cc8730ac316e1903d5fa10ac28da9aea546d5
Robin Altena <altenarobin@gmail.com>
2013-10-03 21:04:40 +0200

    added a test for the registation process

--- src/com/th5/tests/domain/service/AuctionServiceTest.java
+++ src/com/th5/tests/domain/service/AuctionServiceTest.java
@@ -2,6 +2,10 @@ package com.th5.tests.domain.service;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import oracle.sql.DATE;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -10,6 +14,7 @@ import com.th5.domain.model.User;
 import com.th5.domain.other.AuctifyException;
 import com.th5.domain.service.AuctionServiceInterface;
 import com.th5.domain.service.ServiceProvider;
+import com.th5.persistance.UserDatabaseCRUD;
 
 public class AuctionServiceTest {
 	
@@ -24,7 +29,7 @@ public class AuctionServiceTest {
 	@Test
 	public void loginTest(){
 		try {
-			User user = service.login("admin", "admin");
+			User user = service.login("testaccount@auctify.com", "Apenbeestje1");
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
@@ -32,11 +37,36 @@ public class AuctionServiceTest {
 	
 	@Test (expected = AuctifyException.class)
 	public void loginTestUnexistingUser() throws AuctifyException{
-		User user = service.login("dakoika", "admin");
+		User user = service.login("dakoika", "Apenbeestje1");
 	}
 	
 	@Test (expected = AuctifyException.class)
 	public void loginTestIncorrectPassword() throws AuctifyException{
-		User user = service.login("admin", "test");
+		User user = service.login("testaccount@auctify.com", "test");
+	}
+	
+	@Test
+	public void registerTest(){
+		try {
+			service.register("testaccount2@auctify.com", "Apenbeestje1", "Test Account", "Test voornaam", "Test achternaam", 1, new Date(0), "1111TT", "2", "Test", "Test");
+		} catch (AuctifyException e) {
+			fail("Failed to register user");
+		}
+		UserDatabaseCRUD crud = new UserDatabaseCRUD();
+		
+		User user = null;
+		
+		try {
+			user = crud.retrieve("testaccount2@auctify.com");
+		} catch (AuctifyException e) {
+			fail("failed to retrieve user");
+		}
+		if (user == null)fail("failed to retrieve user"); 
+		
+		try {
+			crud.delete(user);
+		} catch (AuctifyException e) {
+			fail("failed to delete user");
+		}
 	}
 }
