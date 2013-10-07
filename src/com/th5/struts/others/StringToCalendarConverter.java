e8297ce2d88f841c7c8c66e17c0785f575706fac
Robin Altena <altenarobin@gmail.com>
2013-10-08 00:48:22 +0200

    Added StringToCalendarConverter. this should replace the StringToDateTimeConver once we dont use it anymore

--- src/com/th5/struts/others/StringToCalendarConverter.java
+++ src/com/th5/struts/others/StringToCalendarConverter.java
@@ -3,7 +3,6 @@ package com.th5.struts.others;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 
@@ -11,7 +10,7 @@ import org.apache.struts2.util.StrutsTypeConverter;
 
 import com.opensymphony.xwork2.conversion.TypeConversionException;
 
-public class StringToDateTimeConverter extends StrutsTypeConverter {
+public class StringToCalendarConverter extends StrutsTypeConverter {
 
     private static final DateFormat DATETIME_FORMAT = new SimpleDateFormat("MM/dd/yyyy");
 
@@ -21,17 +20,15 @@ public class StringToDateTimeConverter extends StrutsTypeConverter {
         }
 
         try {
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(DATETIME_FORMAT.parse(strings[0]));
-            return calendar;
+            return DATETIME_FORMAT.parse(strings[0]);
         } catch (ParseException e) {
             throw new TypeConversionException("Unable to convert given object to date: " + strings[0]);
         }
     }
 
-    public String convertToString(Map context, Object calendar) {
-        if (calendar != null && calendar instanceof Calendar) {         
-            return DATETIME_FORMAT.format(calendar);
+    public String convertToString(Map context, Object date) {
+        if (date != null && date instanceof Date) {         
+            return DATETIME_FORMAT.format(date);
         } else {
             return null;
         }
