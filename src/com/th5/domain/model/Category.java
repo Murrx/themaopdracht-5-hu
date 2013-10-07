38934a139734e06b76131b83e8f8de03820f5645
Robin Altena <altenarobin@gmail.com>
2013-10-08 00:46:44 +0200

    some changes to Category to allow it to be printed in lowercase

--- src/com/th5/domain/model/Category.java
+++ src/com/th5/domain/model/Category.java
@@ -1,3 +1,13 @@
 package com.th5.domain.model;
 
-public enum Category {ELECTRONICS,BOOKS,CARS}
+public enum Category {ELECTRONICS("Electronics"),BOOKS("Books"),CARS("Cars");
+	private String name;
+	
+	Category(String name){
+		this.name = name;
+	}
+	
+	public String toString(){
+		return name;
+	}
+}
