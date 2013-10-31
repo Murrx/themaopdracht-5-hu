2228f305e64fd4e662de5a5d977d1a31121a69e6
Robin Altena <altenarobin@gmail.com>
2013-10-31 21:15:24 +0100

    added constructor to restore bids from db and included some javadocs

--- src/com/th5/domain/model/Bid.java
+++ src/com/th5/domain/model/Bid.java
@@ -3,6 +3,8 @@ package com.th5.domain.model;
 import java.util.Calendar;
 
 import com.th5.domain.other.AuctifyException;
+import com.th5.domain.other.AuctionListManager;
+import com.th5.domain.service.ServiceProvider;
 import com.th5.persistance.BidDatabaseCRUD;
 
 public class Bid implements Comparable<Bid> {
@@ -14,8 +16,14 @@ public class Bid implements Comparable<Bid> {
 	private Calendar timestamp;
 	private int bidAmount;
 
-	public Bid(User user, Auction auction, int bidAmount)
-			throws AuctifyException {
+	
+	/**Use this constructor to create new Bids
+	 * @param user that creates the bid
+	 * @param auction the auction the bid is placed on
+	 * @param bidAmount 
+	 * @throws AuctifyException
+	 */
+	public Bid(User user, Auction auction, int bidAmount) throws AuctifyException {
 		this.user = user;
 		this.auction = auction;
 		this.bidAmount = bidAmount;
@@ -24,6 +32,22 @@ public class Bid implements Comparable<Bid> {
 
 	}
 
+	/**Use this constructor to restore bids from database
+	 * @param bidId
+	 * @param userId of the user that created the bid
+	 * @param auctionId of the auction the bid belongs to
+	 * @param timestamp
+	 * @param bidAmount
+	 * @throws AuctifyException 
+	 */
+	public Bid(int bidId, int userId, int auctionId, Calendar timestamp, int bidAmount) throws AuctifyException{
+		this.bid_Id = bidId;
+		this.user = ServiceProvider.getService().getUserById(userId);
+		this.auction = AuctionListManager.getAuctionById(auctionId);
+		this.timestamp = timestamp;
+		this.bidAmount = bidAmount;
+	}
+	
 	public void refundBidCoins() throws AuctifyException {
 		user.addBidCoins(bidAmount);
 	}
