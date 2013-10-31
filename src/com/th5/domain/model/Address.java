fd95750ac2138d8574e5f77ed1e16534f71f3c13
Dimiter Geelen <mrdidel@gmail.com>
2013-10-31 11:19:51 +0100

    Fix Tests
    Alle tests werken weer zoals het hoort

--- src/com/th5/domain/model/Address.java
+++ src/com/th5/domain/model/Address.java
@@ -1,20 +1,9 @@
 package com.th5.domain.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.th5.domain.observation.Observable;
-import com.th5.domain.observation.Observer;
-import com.th5.domain.other.AuctifyException;
-
-public class Address implements Observable {
+public class Address {
 	
 	private String postalCode, houseNumber, street, city;
 	private int id;
-	
-	private List<Observer> observers;
-    private final Object MUTEX= new Object();
-    private boolean changed;
 	
 	public Address(String postalCode, String houseNumber, String street, String city){
 		this.postalCode = postalCode;
@@ -32,107 +21,31 @@ public class Address implements Observable {
 		return postalCode;
 	}
 
-	public void setPostalCode(String postalCode) throws AuctifyException {
-		String oldPostalCode = this.postalCode;
-		try {
-			this.postalCode = postalCode;
-			this.changed = true;
-			notifyObservers();
-		} catch (AuctifyException e) {
-			this.postalCode = oldPostalCode;
-			this.changed = false;
-			throw new AuctifyException(e.getMessage());
-		}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 
 	public String getHouseNumber() {
 		return houseNumber;
 	}
 
-	public void setHouseNumber(String houseNumber) throws AuctifyException {
-		String oldHouseNumber = this.houseNumber;
-		try {
-			this.houseNumber = houseNumber;
-			this.changed = true;
-			notifyObservers();
-		} catch (AuctifyException e) {
-			this.houseNumber = oldHouseNumber;
-			this.changed = false;
-			throw new AuctifyException(e.getMessage());
-		}
+	public void setHouseNumber(String houseNumber) {
+		this.houseNumber = houseNumber;
 	}
 
 	public String getStreet() {
 		return street;
 	}
 
-	public void setStreet(String street) throws AuctifyException {
-		String oldStreet = this.street;
-		try{
-			this.street = street;
-			this.changed = true;
-			notifyObservers();
-		} catch (AuctifyException e) {
-			this.street = oldStreet;
-			this.changed = false;
-			throw new AuctifyException(e.getMessage());
-		}
+	public void setStreet(String street) {
+		this.street = street;
 	}
 
 	public String getCity() {
 		return city;
 	}
 
-	public void setCity(String city) throws AuctifyException {
-		String oldCity = this.city;
-		try	{
-			this.city = city;
-			this.changed = true;
-			notifyObservers();
-		} catch (AuctifyException e) {
-			this.city = oldCity;
-			this.changed=false;
-			throw new AuctifyException(e.getMessage());
-		}
-	}
-	
-	
-	@Override
-	public void register(Observer obs) {
-		System.out.println("Register obs");
-
-		if(obs == null) throw new NullPointerException("Observer is Null");
-		if(!observers.contains(obs)) observers.add(obs);
-	}
-	@Override
-	public void unregister(Observer obs) {
-		// TODO Auto-generated method stub
-        observers.remove(obs);
-		
-	}
-	@Override
-	public void notifyObservers() throws AuctifyException {
-		// TODO Auto-generated method stub
-		List<Observer> observersLocal = null;
-		//synchronization is used to make sure any observer registered after message is received is not notified
-		synchronized (MUTEX) {
-			if (!changed)
-				return;
-			observersLocal = new ArrayList<Observer>(this.observers);
-			this.changed=false;
-		}
-	for (Observer obs : observersLocal) {
-			try {
-				obs.updateObserver(this);
-			} catch (AuctifyException e) {
-				// TODO Auto-generated catch block
-				throw new AuctifyException(e.getMessage());
-			}
-		}
-	}
-	@Override
-	public Object getUpdate(Observer obs) {
-		// TODO Auto-generated method stub
-		return (Address) this;
+	public void setCity(String city) {
+		this.city = city;
 	}
 }
