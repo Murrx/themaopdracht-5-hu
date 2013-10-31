a4ee12252cc73e649936ad2589d061b8d5e9c4d1
Robin Altena <altenarobin@gmail.com>
2013-10-31 22:16:05 +0100

    Small fix to DatabaseSyncList and added DatabaseSyncMap

--- src/com/th5/domain/other/DatabaseSyncMap.java
+++ src/com/th5/domain/other/DatabaseSyncMap.java
@@ -2,9 +2,7 @@ package com.th5.domain.other;
 
 import java.util.List;
 
-import com.th5.domain.model.Bid;
-
-public interface DatabaseSyncList<T> {
+public interface DatabaseSyncMap<T> {
 	public void add(T t) throws AuctifyException;
 	public boolean isEmpty();
 	public int size();
