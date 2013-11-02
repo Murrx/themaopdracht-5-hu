9f057bbc1b6b3e019a03bed7691818ed60e2e514
Robin Altena <altenarobin@gmail.com>
2013-11-02 22:07:45 +0100

    small name fix in sortedArrayList

--- src/com/th5/domain/util/SortedArrayList.java
+++ src/com/th5/domain/util/SortedArrayList.java
@@ -3,12 +3,13 @@ package com.th5.domain.util;
 import java.util.ArrayList;
 import java.util.Collections;
 
+@SuppressWarnings("serial")
 public class SortedArrayList<T extends Comparable<T>> extends ArrayList<T>{
 	
-	public boolean add(T user) {
-        int index = Collections.binarySearch(this, user);
+	public boolean add(T type) {
+        int index = Collections.binarySearch(this, type);
         if (index < 0) index = ~index;
-        super.add(index, user);
+        super.add(index, type);
         return true;
     }
 
