3a0febd7a64fd4c73a9ac51397db55aee2fd8c25
Dimiter Geelen <mrdidel@gmail.com>
2013-11-06 00:55:16 +0100

    Observation Javadocs

--- src/com/th5/domain/observation/Observable.java
+++ src/com/th5/domain/observation/Observable.java
@@ -2,15 +2,51 @@ package com.th5.domain.observation;
 
 import com.th5.domain.other.AuctifyException;
 
+/**
+ * themaopdracht5 - Auctify<br>
+ * This interface contains the methods for the Observable. An observable is a class that is observed by
+ * an observer. The observable notifies the observer whenever it changes.
+ * When a change is detected, the method notifyObservers() is called.
+ * @author <b>GarbageCollectors 2.0 </b>(Dimiter Geelen, Mark Van Lagen, Martin Bakker, Joris Rijkes and Robin Altena)
+ * @see Observer 
+ */
 public interface Observable {
 	
-	//methods to register and unregister observers
+	/**
+	 * themaopdracht5 - Auctify<br>
+	 * This method registers a new observer for the observable. The observer will be notified if the
+	 * notifiedObservers() method is called.
+	 * @param obs The new observer to register
+	 * @author <b>GarbageCollectors 2.0 </b>(Dimiter Geelen, Mark Van Lagen, Martin Bakker, Joris Rijkes and Robin Altena)
+	 * @see #unregister
+	 */
 	public void register(Observer obs);
+	
+	/**
+	 * themaopdracht5 - Auctify<br>
+	 * This method removes an observer from the observable. The observer will not be notified anymore if the
+	 * notifiedObservers() method is called.
+	 * @param obs The observer to remove
+	 * @author <b>GarbageCollectors 2.0 </b>(Dimiter Geelen, Mark Van Lagen, Martin Bakker, Joris Rijkes and Robin Altena)
+	 * @see #register
+	 */
 	public void unregister(Observer obs);
 
-	//method to notify observers of change
+	/**
+	 * themaopdracht5 - Auctify<br>
+	 * This method will be called whenever the observers need to be notified of a change. All the observers
+	 * that are registered with the register() method are notified.
+	 * @throws AuctifyException should be thrown when the method fails to notify the observers.
+	 * @author <b>GarbageCollectors 2.0 </b>(Dimiter Geelen, Mark Van Lagen, Martin Bakker, Joris Rijkes and Robin Altena)
+	 * @see #register
+	 */
 	public void notifyObservers() throws AuctifyException;
 
-	//method to get updates from subject
+	/**
+	 * themaopdracht5 - Auctify<br>
+	 * Gets the updated observable object from a specific observer.
+	 * @param obs The observer to get the update from
+	 * @author <b>GarbageCollectors 2.0 </b>(Dimiter Geelen, Mark Van Lagen, Martin Bakker, Joris Rijkes and Robin Altena)
+	 */
 	public Object getUpdate(Observer obs);
 }
--- src/com/th5/domain/observation/Observer.java
+++ src/com/th5/domain/observation/Observer.java
@@ -2,11 +2,30 @@ package com.th5.domain.observation;
 
 import com.th5.domain.other.AuctifyException;
 
+/**
+ * themaopdracht5 - Auctify<br>
+ * This interface contains the methods for the Observer. An observer is a class that observes
+ * an observable. The observer is responsible for handling changes in the observable object.
+ * Whenever a change is detected, the method updateObserver() is called.
+ * @author <b>GarbageCollectors 2.0 </b>(Dimiter Geelen, Mark Van Lagen, Martin Bakker, Joris Rijkes and Robin Altena)
+ * @see Observable 
+ */
 public interface Observer {
 
-	//method to update the observer, used by subject
+	/**
+	 * themaopdracht5 - Auctify<br>
+	 * This method is responsible for updating the object that has been changed.
+	 * @param obj The object that has changed
+	 * @throws AuctifyException should be thrown when the method fails to update the observer
+	 * @author <b>GarbageCollectors 2.0 </b>(Dimiter Geelen, Mark Van Lagen, Martin Bakker, Joris Rijkes and Robin Altena)
+	 */
 	public void updateObserver(Object obj) throws AuctifyException;
 
-	//attach with subject to observe
+	/**
+	 * themaopdracht5 - Auctify<br>
+	 * This method sets the observable object that should be observed by the observer.
+	 * @param obs The observable object that has to be observed
+	 * @author <b>GarbageCollectors 2.0 </b>(Dimiter Geelen, Mark Van Lagen, Martin Bakker, Joris Rijkes and Robin Altena)
+	 */
 	public void setObservable (Observable obs);
 }
