98be85646ad619d3c4f1d6cbfc8b1727d3093c35
Robin Altena <altenarobin@gmail.com>
2014-03-03 09:47:33 +0100

    Yay

---
+++ src/com/th5/tests/domain/model/UserTest.java
@@ -0,0 +1,179 @@
+package com.th5.tests.domain.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void testUserInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserStringStringStringUserRights() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUserIntStringStringStringUserRightsInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateAuction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDisplayName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDisplayName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRights() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRights() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBidCoins() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddBidCoins() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMyAuctions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTakeBidCoins() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnregister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyObservers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBidOnAuction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMyBids() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveAuction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIdentifier() {
+		fail("Not yet implemented");
+	}
+
+}
